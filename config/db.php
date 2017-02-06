4a30e5b1aabd2488e2471f1039e3473bc27fca7d
Ionuț Popescu <Ionu Popescu>
2017-02-06 12:54:34 +0200

    Last version

--- config/db.php
+++ config/db.php
@@ -6,11 +6,11 @@
 	try {
 		$account = new PDO("mysql:host=$host;dbname=account", $user, $password);
 	} catch(PDOException $e) {
-		die('Conexiunea la baza de date este imposibila:' . $e);
+		die('Conexiunea la baza de date este imposibila.');
 	}
 	 
 	try {
 		$player = new PDO("mysql:host=$host;dbname=player", $user, $password);
 	} catch(PDOException $e) {
-		die('Conexiunea la baza de date este imposibila:' . $e);
+		die('Conexiunea la baza de date este imposibila.');
 	}
